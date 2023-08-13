a242ee6268af2f630804801bbbf43be8275dbfa0
Lesnikovich <68425826+Lesnikovich@users.noreply.github.com>
2023-08-14 01:31:12 +0300

    The site has been adapted. Favicon added.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
 });
+
